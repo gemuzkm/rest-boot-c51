da562efe90d520e82aeba9d2eae0941d38710868
gemuz_work <gemuzkm@gmail.com>
2022-03-30 12:14:02 +0300

    Initial commit

---
+++ src/main/java/com/example/restbootc51/RestBootC51Application.java
@@ -0,0 +1,13 @@
+package com.example.restbootc51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBootC51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBootC51Application.class, args);
+    }
+
+}
