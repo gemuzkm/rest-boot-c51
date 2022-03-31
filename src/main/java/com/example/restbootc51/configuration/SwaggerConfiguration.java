c7998ac3c57297eddd82645935e42430c002fc95
gemuzkm <gemuzkm@gmail.com>
2022-03-31 22:04:19 +0300

    add swagger ApiInfo

--- src/main/java/com/example/restbootc51/configuration/SwaggerConfiguration.java
+++ src/main/java/com/example/restbootc51/configuration/SwaggerConfiguration.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -15,6 +19,17 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.restbootc51.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My REST API",
+                "Calculator API.",
+                "API TOS",
+                "Terms of service",
+                new Contact("Gemuzkm", "http://localhost:8080", "gemuzkm@gmail.com"),
+                "License of API", "/", Collections.emptyList());
     }
 }
