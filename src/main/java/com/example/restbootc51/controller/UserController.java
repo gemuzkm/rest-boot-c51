6f2940b3dbb1a5abf785a99f48b0fc5612b9fe33
gemuz_work <gemuzkm@gmail.com>
2022-03-31 10:28:28 +0300

    class UserController init

---
+++ src/main/java/com/example/restbootc51/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.example.restbootc51.controller;
+
+import com.example.restbootc51.entity.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @PostMapping
+    public User save(@RequestBody User user) {
+
+    }
+}
--- src/main/java/com/example/restbootc51/entity/User.java
+++ src/main/java/com/example/restbootc51/entity/User.java
@@ -1,18 +1,22 @@
 package com.example.restbootc51.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "USERS")
-@Data
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String username;
     private String password;
+    private String name;
 }
