6b036f3c79d62f78a19efe0d73b9342fc9c8d600
gemuz_work <gemuzkm@gmail.com>
2022-03-31 10:43:16 +0300

    class UserController - make sample

--- src/main/java/com/example/restbootc51/controller/UserController.java
+++ src/main/java/com/example/restbootc51/controller/UserController.java
@@ -1,17 +1,39 @@
 package com.example.restbootc51.controller;
 
 import com.example.restbootc51.entity.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.restbootc51.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @PostMapping
-    public User save(@RequestBody User user) {
+    @Autowired
+    private UserRepository userRepository;
 
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {
+       User save =  userRepository.save(user);
+       return ResponseEntity.ok(save);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> all = userRepository.findAll();
+        return ResponseEntity.ok(all);
+    }
+
+    @GetMapping("/findByUserName")
+    public  ResponseEntity<User> findByUsername(String username) {
+        Optional<User> byUsername = userRepository.findByUsername(username);
+        if (byUsername.isPresent()) {
+            return ResponseEntity.ok(byUsername.get());
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
--- src/main/java/com/example/restbootc51/repository/UserRepository.java
+++ src/main/java/com/example/restbootc51/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.restbootc51.repository;
 import com.example.restbootc51.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
+
