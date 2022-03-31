ed4de33b023c117f0d220293e2252582a7c34145
gemuz_work <gemuzkm@gmail.com>
2022-03-31 08:47:21 +0300

    class CalculatorController init

---
+++ src/main/java/com/example/restbootc51/controller/CalculatorController.java
@@ -0,0 +1,9 @@
+package com.example.restbootc51.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Component
+public class CalculatorController {
+}
